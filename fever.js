e46792d37c9629d370169631cbc1e67c2008c11c
Wiking <96825472+Waikingen@users.noreply.github.com>
2023-11-30 14:30:20 +0100

    Update fever.js

--- fever.js
+++ fever.js
@@ -1,1 +1,24 @@
+//-------------------------------------
+// WDM MAIN SCRIPT - V1.3
+// CLIENT CASE - FEVER
+//-------------------------------------
 
+console.log("connected");
+
+/* START STOP SCROLL TIMEOUT */
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.SScroll.call.stop();
+});
+
+ScrollTrigger.addEventListener("scrollStart", () => {
+    
+  const trigger = document.getElementById("trigger-hero");
+  trigger.click();
+
+  setTimeout(function () {
+    window.SScroll.call.start();
+  }, 1500); //delay is in milliseconds
+});
+
+/* END STOP SCROLL TIMEOUT */
